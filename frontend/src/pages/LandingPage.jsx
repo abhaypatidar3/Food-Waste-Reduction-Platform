0ecea4f662aa920ffcb126857ca60a9a5bb92b2b
abhaypatidar3 <patidarabhay1996@gmail.com>
2025-12-27 11:33:09 +0530

    Landing page enhancement

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { Leaf, Users, TrendingUp, MapPin, Clock, Heart } from 'lucide-react';
+import Card from '../components/Landingpage/Card';
+
 const LandingPage = () => {
   return (
     <div className='min-h-screen'>
-        <header className="bg-gradient-to-r from-[#1E7F5C] to-[#166651] text-white w-[100vw]">
-            <nav className='flex justify-between mx-2 px-6 py-4 items-center w-[100vw]'>
-              <div>
+        <header className="bg-gradient-to-r from-[#1E7F5C] to-[#166651] text-white mx-0">
+            <nav className='flex justify-between mx-0 py-4 items-center'>
+              <div className='ml-5'>
+                <Leaf className='inline-block w-6 mr-2'/>
                 <span>FoodShare</span>
               </div>
               <div>
                 <div className='flex gap-4'>
                   <Link to='/login' className='hover:bg-white/10 px-4 py-2 rounded-md'>Login</Link>
-                  <Link to='/signup' className='bg-[#F4A261] text-white hover:bg-[#E89350] shadow-sm px-4 py-2 rounded-md'>Sign Up</Link>
+                  <Link to='/signup' className='bg-[#F4A261] text-white hover:bg-[#E89350] shadow-sm px-4 py-2 rounded-md mr-5'>Sign Up</Link>
                 </div>
               </div>
             </nav>
-
-            <div className='py-20 px-6 text-center'>
+            {/* hero section */}
+            <div className='py-20 mx-0 text-center'>
                 <h1 className="text-5xl mb-6 max-w-4xl mx-auto">
                   Transform Surplus Food into Hope
                 </h1>
@@ -30,16 +33,24 @@ const LandingPage = () => {
               </div>
             </div>
         </header>
-        <div className='py-20 text-center bg-slate-300 grid grid-cols-1 gap-10 md:grid-cols-3'>
+        <div className='py-20 text-center bg-white grid grid-cols-1 gap-10 md:grid-cols-3'>
           <div className='flex pl-[10vw]'>
             <Heart className='w-[50px] text-[#1E7F5C]' />
-            <p>Meals Saved</p>
+            <p>Meals Served</p>
           </div>
           <div>
             NGOs Connected
           </div>
           <div>
             Food Waste Prevented
+          </div>
+        </div>
+        <div className='h-[50vh] bg-slate-100 shadow-lg border border-slate-100 justify-items-center items-center'>
+          <h2 className='text-3xl text-center mb-12 text-[#2E2E2E] pt-[5vh]'>How it Works?</h2>
+          <div className='grid grid-cols-1 gap-10 md:grid-cols-3'>
+          <Card step={"1"} icon={<Leaf className='w-12 h-12 text-[#1E7F5C] '/>} desc={"Restaurants post available food with quantity, pickup time, and location details"}/>
+          <Card step={"2"} icon={<MapPin className='w-12 h-12 text-[#F4A261] '/>} desc={"Nearby NGOs get notified and can accept donations in real-time"}/>
+          <Card step={"3"} icon={<Clock className='w-12 h-12 text-[#1E7F5C] '/>} desc={"Track pickup status with QR verification and build impact analytics"}/>
           </div>
         </div>
     </div>
